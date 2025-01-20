9c0b7b0330472dfda277c78804e26872f0e397e7
SakshamKaundal <137274099+SakshamKaundal@users.noreply.github.com>
2025-01-21 00:49:51 +0530

    alignment again

--- src/pages/Radar2.tsx
+++ src/pages/Radar2.tsx
@@ -8,10 +8,10 @@ import { Arrow } from "../components/Arrow";
 export function Radar2() {
     const navigate = useNavigate()
   return (
-    <div className="h-screen overflow-hidden bg-black w-full max-h-full relative bg-center bg-[url(/images/radar3.png)] font-rock2 bg-100 bg-no-repeat">     
+    <div className="h-screen overflow-hidden bg-black w-full max-h-full relative bg-center bg-[url(/images/rf.png)] font-rock2 bg-100 bg-no-repeat">     
         <AnimatedButton icon={<BackIcon/>} onclick={()=>{navigate('/map')}}/>
         
-        <div className="absolute left-[23%] top-1/2 z-2">
+        <div className="absolute left-[18%] top-1/2 z-2">
           <Button3D onclick={()=>{navigate('/map2')}}>Parallel Index Line</Button3D>
         </div>
         <div className="absolute left-[2%] bottom-[8%] z-2">
@@ -20,11 +20,11 @@ export function Radar2() {
         <div className="absolute left-[2%] bottom-[2%] z-2">
                 <Button3D className="bg-green-400 text-white border-b-4 border-green-700 hover:bg-green-500" onclick={()=>{navigate('/list')}}>Index</Button3D>
         </div>
-        <div className="absolute right-[22%] top-[32%] z-2">
+        <div className="absolute right-[18%] top-[32%] z-2">
           <Button3D onclick={()=>{navigate('/map-c-ranges')}}>Clearing Ranges <br /> (NLT and NMT) </Button3D>
         </div>
         
-        <div className="absolute right-[22%] top-[56%] z-2">
+        <div className="absolute right-[18%] top-[56%] z-2">
           <Button3D onclick={()=>{navigate('/map-wheelover')}}>Wheel Over PIL</Button3D>
         </div>
         
