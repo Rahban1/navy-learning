9b4cf901618d477c44232149f7a76a2be30fec46
SakshamKaundal <137274099+SakshamKaundal@users.noreply.github.com>
2025-01-27 21:24:42 +0530

    hopefully last commit

--- src/pages/Bridge.tsx
+++ src/pages/Bridge.tsx
@@ -45,7 +45,7 @@ export function Bridge() {
       <div className="bg-blue-500 border-white border-2 bg-opacity-75 text-white text-3xl font-bold font-rock2 z-4 absolute top-12 left-1/2 transform -translate-x-1/2 p-4 rounded-lg shadow-lg">Welcome to the bridge. Hover on the equipment/topic you would like to learn about</div>
       <audio ref={audioRef} src='./audio/AudioBridge.mp3'/>
       <div 
-          className=' w-36 h-20 z-20 absolute left-[39vw] top-[19vw]'
+          className=' w-36 h-20 z-20 cursor-pointer absolute left-[39vw] top-[19vw]'
           onMouseEnter={()=>handleMouseEnter('Blind Pilotage')}
           onMouseLeave={handleMouseLeave}
           onClick={()=>{navigate('/list')}}
@@ -70,17 +70,17 @@ export function Bridge() {
       <AnimatePresence>
   {hoveredItem && (
     <motion.div
-      className="z-10 absolute left-[34%] top-[22%] w-[17%] h-[30%] bg-black shadow-lg p-4 rounded-lg overflow-hidden"
+      className="z-10 absolute cursor-pointer left-[34%] top-[22%] w-[17%] h-[30%] bg-black shadow-lg p-4 rounded-lg overflow-hidden"
       style={{ cursor: 'pointer' }} // Inline style for cursor
       initial={{ scale: 0.3, opacity: 0 }}
       animate={{ scale: 1, opacity: 1 }}
       exit={{ scale: 0.3, opacity: 0 }}
       transition={{ type: "spring", stiffness: 100, damping: 15 }}
     >
-      <div className='flex justify-center items-center'>
+      <div className='flex justify-center cursor-pointer items-center'>
         <img
           src="/images/radar-final.jpeg"
-          className=" max-w-full"
+          className=" max-w-full cursor-pointer"
           alt="Background"
         />
       </div>
